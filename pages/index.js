2b522ae023eaf60fbef092028c6360bcd75085e8
si-shakil <sishakil850@gmail.com>
2023-01-25 23:35:36 +0600

    second section adde

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,30 @@
 import Head from "next/head";
 import Navbar from "../src/components/Navbar";
+import Banner from "../src/sections/homepage/Banner";
+import VariableWidthSlick from "../src/sections/homepage/variableWidthSlick";
 
 function Home() {
   return (
-    <div
-      style={{
-        backgroundImage: `url(https://i.ibb.co/R6dhPJh/furnix-banner.webp)`,
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center center",
-      }}
-      className="h-[100vh]"
-    >
+    <div>
       <Head>
         <title>Home | Furnix</title>
         <meta property="description" content="Homepage" />
       </Head>
-      <Navbar />
-
-      <div className="flex flex-col justify-center items-center pt-5">
-        <h1 className="text-primary/50 text-2xl">WELCOME TO FURNIX</h1>
-        <h1 className="text-primary text-6xl pt-4">Art with an attitude</h1>
-        <button className="bg-secondary px-3 py-2 rounded-full hover:bg-primary mt-7 font-bold">
-          Discover Now
-        </button>
+      {/* First section of home page */}
+      <div
+        style={{
+          backgroundImage: `url(https://i.ibb.co/R6dhPJh/furnix-banner.webp)`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center center",
+        }}
+        className="h-[100vh]"
+      >
+        <Navbar />
+        <Banner />
       </div>
+      {/* second section: variable width slick slider */}
+      <VariableWidthSlick />
     </div>
   );
 }
